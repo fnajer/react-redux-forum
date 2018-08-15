8a19059af925d3e949b8d7c84f95c7f5d108263a
fnajer <lexactr@gmail.com>
2018-08-15 22:07:36 +0300

    Create store && Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, withRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter, withRouter } from 'react-router-dom';
+import store from './store';
 
 const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <AppWithRouter />
-  </BrowserRouter>  
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppWithRouter />
+    </BrowserRouter>  
+  </Provider>
 , document.getElementById('root'));
