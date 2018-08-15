96ec6b126dcc74adc25c405d7556d9d103091388
fnajer <lexactr@gmail.com>
2018-08-15 22:26:33 +0300

    Test devtools Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,25 @@
 import { createStore } from 'redux';
 
-const reducer = (state = {}, action) => {
+const reducer = (state, action) => {
+  if (action.type === 'new_action') {
+    return [{
+      text: 'hello',
+      id: 1,
+    }];
+  }
   return state;
 }
 
-const store = createStore(reducer);
+const initialState = {
+  channels: [],
+};
 
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+store.dispatch({
+  type: 'new_action'
+});
 export default store;
